da1bb1fd5f752e18bcead14001baf4ce44fa8916
WeirdmanSMR <maga.saykhanov95@gmail.com>
2023-10-26 00:33:29 +0300

    Чуть нормализовал код страницы авторизации и установил Rtk

--- src/components/AuthBlock/AuthBlock.jsx
+++ src/components/AuthBlock/AuthBlock.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import AuthInput from "./AuthInput/AuthInput";
+import { Link } from "react-router-dom";
+import style from "./AuthBlock.module.scss";
 
 const AuthBlock = () => {
-  const [username, setUsername] = useState("");
+	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 
 	const handleLogin = () => {
@@ -14,55 +16,27 @@ const AuthBlock = () => {
 	};
 
 	return (
-		<div
-			style={{
-				display: "flex",
-				alignItems: "center",
-				gap: 20,
-				flexDirection: "column",
-				backgroundColor: "white",
-				borderRadius: 20,
-				padding: "40px 20px",
-				border: "1px solid #112d4e",
-			}}
-		>
-			<h1 style={{ color: "#112d4e", fontSize: 40 }}>Авторизация</h1>
-			<form
-				method="post"
-				style={{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-				}}
-			>
+		<div className={style.authCont}>
+			<h1>Авторизация</h1>
+			<form method="get">
 				<AuthInput
 					type="text"
 					placeholder="Имя"
 					value={username}
 					onChange={(e) => setUsername(e.target.value)}
 				/>
-				<br />
 				<AuthInput
 					type="password"
 					placeholder="Пароль"
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
 				/>
-				<br />
-				<button
-					style={{
-						outline: "none",
-						border: "none",
-						backgroundColor: "#112d4e",
-						color: "white",
-						padding: "10px 20px",
-						fontSize: 18,
-						borderRadius: 10,
-					}}
-					onClick={handleLogin}
-				>
-					Войти
-				</button>
+				<div className={style.btnCont}>
+					<button id={style.login} onClick={handleLogin}>
+						Войти
+					</button>
+					<button id={style.registration}>Регистрация</button>
+				</div>
 			</form>
 		</div>
 	);
